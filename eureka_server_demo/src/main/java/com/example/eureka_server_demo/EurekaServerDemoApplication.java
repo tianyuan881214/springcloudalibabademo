4f3aa3066205419ab12ccc9127eb20641712cdce
曹少军 <tianyuan881214@sina.com>
2021-04-01 21:57:55 +0800

    tian jia eureka server project

---
+++ eureka_server_demo/src/main/java/com/example/eureka_server_demo/EurekaServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka_server_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerDemoApplication.class, args);
+    }
+
+}
---
+++ eureka_server_demo/src/test/java/com/example/eureka_server_demo/EurekaServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_server_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

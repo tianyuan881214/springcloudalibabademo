d246d4a4cb74f2bd0bc6836b616f5e505fa7e4dd
曹少军 <tianyuan881214@sina.com>
2021-04-01 19:28:29 +0800

    Initial commit

---
+++ src/test/java/com/example/springcloudalibabademo/SpringcloudalibabademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudalibabademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudalibabademoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
